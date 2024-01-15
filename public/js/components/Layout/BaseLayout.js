d5767565c32aad48f88ea9cc15036f2a1069915b
Cris <hainberg@technikum-wien.at>
2024-01-15 15:41:22 +0100

    Created BaseLayout Component
    BaseLayout Component provides
    . title / subtitle properties
    . main slot for main content:
      default width = 12 grid cols. Can be changed.
    . aside slot for side content (status, kontakte, faqs,...)
      default width = 0 grid cols. Can be changed.

---
+++ public/js/components/Layout/BaseLayout.js
@@ -0,0 +1,35 @@
+export const BaseLayout = {
+	props: {
+		title: '',
+		subtitle: '',
+		mainCols: [],
+		asideCols: [],
+		alignToCoreNav: false,
+	},
+	computed: {
+		mainGridCols() {
+			return this.mainCols.length > 0 ? `col-md-${this.mainCols[0]}` : "col-md-12";
+		},
+		asideGridCols() {
+			return this.asideCols.length > 0 ? `col-md-${this.asideCols[0]}` : "";
+		},
+		customAlign() {
+			return this.alignToCoreNav ? 'align-to-coreNav' : '';
+		}
+	},
+	template: `
+	<div class="overflow-hidden" :class="customAlign">
+		<header v-if="title">
+			<h1 class="h2">{{ title }}<span class="fhc-subtitle">{{ subtitle }}</span></h1>
+		</header>
+		<div class="row gx-3">
+			<main :class="mainGridCols">
+				<slot name="main">{{ mainGridCols }}</slot>
+			</main>
+			<aside v-if="asideCols.length > 0" :class="asideGridCols">
+				<slot name="aside">{{ asideGridCols }}</slot>
+			</aside>
+		</div>
+	</div>
+	`
+};
