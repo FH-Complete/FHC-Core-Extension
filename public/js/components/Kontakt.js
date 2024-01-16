82e1fbd4483ef8579481438eaaa01c2023ba1fad
Cris <hainberg@technikum-wien.at>
2024-01-16 16:33:35 +0100

    Added padding and increased margins in Kontakte

--- public/js/components/Kontakt.js
+++ public/js/components/Kontakt.js
@@ -27,11 +27,20 @@ export const Kontakt = {
 			<div class="card">
 				<div class="card-header card-title"><h5>Kontakt</h5></div>
 				<ul class="list-group list-group-flush">
-					<li class="list-group-item" v-for="kontakt in kontakte" :key="kontakt">
-						<div v-show="kontakt.nachname !== ''"><i class="fa fa-user me-3"></i>{{ kontakt.vorname }} {{ kontakt.nachname}}</div>
-						<div v-show="kontakt.email !== ''"><i class="fa fa-envelope me-3"></i><a :href="'mailto:' + kontakt.email" class="text-reset">{{ kontakt.email }}</a></div>
-						<div v-show="kontakt.phone !== ''"><i class="fa fa-phone me-3"></i>{{ kontakt.phone }}</div>
-						<div v-show="kontakt.raum !== ''"><i class="fa fa-location-dot me-3"></i>{{ kontakt.raum }}</div>
+					<li class="list-group-item p-4" v-for="kontakt in kontakte" :key="kontakt">
+						<div v-show="kontakt.nachname !== undefined && kontakt.nachname !== ''">
+							<i class="fa fa-user me-4 mb-2"></i>{{ kontakt.vorname }} {{ kontakt.nachname}}
+						</div>
+						<div v-show="kontakt.email !== undefined && kontakt.email !== ''">
+							<i class="fa fa-envelope me-4 mb-2"></i>
+							<a :href="'mailto:' + kontakt.email" class="text-reset">{{ kontakt.email }}</a>
+						</div>
+						<div v-show="kontakt.phone !== undefined && kontakt.phone !== ''">
+							<i class="fa fa-phone me-4 mb-2"></i>{{ kontakt.phone }}
+						</div>
+						<div v-show="kontakt.raum !== undefined && kontakt.raum !== ''">
+							<i class="fa fa-location-dot me-4 mb-2"></i>{{ kontakt.raum }}
+						</div>
 					</li>
 				</ul>
 			</div>
