8acbc881a60f4c513e99a669e9806f46e249e2b3
Cris <hainberg@technikum-wien.at>
2024-01-24 16:19:37 +0100

    Created Test-Methods getFullName and deleteData

--- controllers/FhcTemplate.php
+++ controllers/FhcTemplate.php
@@ -14,10 +14,13 @@ class FhcTemplate extends Auth_Controller
 	{
 		parent::__construct(
 			array(
-				'index' => 'admin:rw'
+				'index' => 'admin:rw',
+				'getFullName' => 'admin:rw',
+				'deleteData' => 'admin:rw',
 			)
 		);
 	}
+
 
 	// -----------------------------------------------------------------------------------------------------------------
 	// Public methods
@@ -25,5 +28,24 @@ class FhcTemplate extends Auth_Controller
 	public function index()
 	{
 		$this->load->view('extensions/FHC-Core-Extension/FhcTemplate.php');
+	}
+
+	public function getFullName()
+	{
+		$this->load->model('person/Person_model', 'PersonModel');
+		$result = $this->PersonModel->getFullName(getAuthUID());
+
+		if (isError($result))
+		{
+			$this->terminateWithJsonError(getError($result));
+		}
+
+		$this->outputJsonSuccess(hasData($result) ? getData($result) : []);
+	}
+
+	public function deleteData($id)
+	{
+		// Pseudo success object mit retval
+		$this->outputJsonSuccess($id);
 	}
 }
