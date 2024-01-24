3c98157e6bd3ba774beb9c75c8e82abd917d88eb
Cris <hainberg@technikum-wien.at>
2024-01-24 18:44:16 +0100

    Changed main Navigation links

--- config/navigation.php
+++ config/navigation.php
@@ -4,7 +4,7 @@ $config['navigation_header']['*']['FhcTemplate'] = array(
 		'link' => site_url('extensions/FHC-Core-Extension/FhcTemplate'),
 		'icon' => '',
 		'description' => 'FhcTemplate',
-		'sort' => 1
+		'sort' => 100
 	);
 
 // Add Header-Menu-Entry to Extension Page with DropDown
@@ -13,34 +13,8 @@ $config['navigation_header']['extensions/FHC-Core-Extension/FhcTemplate/index'] 
 		'link' => '#',
 		'icon' => '',
 		'description' => 'FHC Template',
-		'sort' => 10,
-		'children'=> array(
-			'sub1' => array(
-				'link' => site_url(),
-				'icon' => '',
-				'description' => 'Submenu 1',
-				'expand' => true,
-				'sort' => 1,
-				'requiredPermissions' => 'basis/vilesci:r'
-			),
-			'sub2' => array(
-				'link' => site_url(),
-				'icon' => 'cubes',
-				'description' => 'Submenu 2',
-				'expand' => true,
-				'sort' => 2,
-				'requiredPermissions' => 'admin:r'
-			)
-		)
+		'sort' => 100
 	));
-
-// Add Side-Menu-Entry to Main Page
-$config['navigation_menu']['Vilesci/index']['administration']['children']['FhcTemplate'] = array(
-		'link' => site_url('extensions/FHC-Core-Extension/FhcTemplate'),
-		'icon' => 'cubes',
-		'description' => 'FHC Example Template',
-		'expand' => true
-);
 
 // Add Side-Menu-Entry to Extension Page
 $config['navigation_menu']['extensions/FHC-Core-Extension/FhcTemplate/index'] = array(
@@ -49,9 +23,9 @@ $config['navigation_menu']['extensions/FHC-Core-Extension/FhcTemplate/index'] = 
 		'description' => 'Zurück',
 		'icon' => 'angle-left'
 	),
-	'Dashboard' => array(
-		'link' => '#',
-		'description' => 'Dashboard',
-		'icon' => 'dashboard'
+	'FHC Template' => array(
+		'link' => site_url('extensions/FHC-Core-Extension/FhcTemplate'),
+		'description' => 'FHC Template',
+		'icon' => ''
 	)
 );
