2f4fdb6f351265851b7002a424635f6052dfcacf
cgfhtw <christopher.gerbrich@technikum-wien.at>
2024-02-13 16:05:37 +0100

    Tables

---
+++ public/js/apps/examples/Tables/Basic.js
@@ -0,0 +1,238 @@
+/**
+ * Copyright (C) 2024 fhcomplete.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+// Load Plugins:
+// ============
+import FhcAlert from "../../../../../../js/plugin/FhcAlert.js";
+
+// Load Helpers:
+// =============
+import { CoreRESTClient } from "../../../../../../js/RESTClient.js";
+
+// Load Components:
+// ===============
+import { CoreFilterCmpt } from "../../../../../../js/components/filter/Filter.js";
+import BsModal from "../../../../../../js/components/Bootstrap/Modal.js";
+
+// Create App:
+// ==========
+const app = Vue.createApp({
+	components: {
+		CoreFilterCmpt,
+		BsModal
+	},
+	methods: {
+		addData() {
+			this.modalTitel = 'Datensatz anlegen';
+			this.$refs.modalContainer.show();
+		},
+		editData(id) {
+			this.modalTitel = 'Datensatz ändern';
+			this.$refs.modalContainer.show();
+		},
+		manipulateData(id) {
+			this.$fhcAlert.alertInfo('ID' + id + ' do some Action');
+		},
+		async deleteData(id) {
+			if (await this.$fhcAlert.confirmDelete() === false)
+				return;
+			this.$fhcAlert.alertSuccess('ID' + id + ' deleted');
+		},
+		acceptData() {
+			this.$fhcAlert.alertSuccess('Accepted');
+		},
+		rejectData() {
+			this.$fhcAlert.alertSuccess('Rejected');
+		}
+	},
+	data: function() {
+		return {
+			modalTitel: '',
+		}
+	},
+	computed: {
+		tabulatorOptions() {
+			return {
+				// Data source
+				ajaxURL: CoreRESTClient._generateRouterURI('/extensions/FHC-Core-Extension/FhcTemplate/getTestData'),
+				ajaxResponse(url, params, response) {
+					return CoreRESTClient.getData(response);
+				},
+				
+				// Unique ID
+				index: 'id',
+				
+				// @see: https://tabulator.info/docs/5.2/layout#layout
+				// This is the default option and can be omitted.
+				layout: 'fitDataStretch',
+				
+				// Column definitions
+				columns: [
+					{
+						formatter: 'rowSelection',
+						titleFormatter: 'rowSelection',
+						titleFormatterParams: {
+							rowRange: "active" // Only toggle the values of the active filtered rows
+						},
+						headerSort: false,
+						frozen: true,
+						width: 70
+					},
+					{title: 'ID', field: 'id', headerFilter: true, width: 70, hozAlign: 'right', frozen: true, sorter: 'number'},
+					{title: 'Core Data', field: 'coreData', headerFilter: true, frozen: true},
+					{title: 'Integer', field: 'number', headerFilter: true, hozAlign: 'right', sorter: 'number'},
+					{title: 'String', field: 'text', headerFilter: true},
+					{title: 'Date', field: 'datum', headerFilter: true, hozAlign: 'center'},
+					{
+						title: 'Boolean',
+						field: 'bool',
+						formatter:"tickCross",
+						headerFilter:"tickCross",
+						headerFilterParams:{"tristate": true},
+						hozAlign:"center",
+						formatterParams: {
+							tickElement: '<i class="fa fa-check text-success"></i>',
+							crossElement: '<i class="fa fa-xmark text-danger"></i>'
+						}
+					},
+					{
+						title:"Money",
+						field:"money",
+						formatter:"money",
+						formatterParams:{
+							decimal:",",
+							thousand:".",
+							symbol:"€",
+							symbolAfter:true
+						},
+						sorter: 'number'
+					},
+					{
+						// TODO umändern, da dms_id eigene row ist in query
+						title: 'File',
+						field: 'datei',
+						headerFilter: true,
+						formatter:"link",
+						formatterParams: cell => {
+							return {
+								labelField:"datei.titel",
+								url: CoreRESTClient._generateRouterURI('extensions/FHC-Core-Extension/FhcTemplate/download/' + cell.getData().datei.dms_id),
+								target:"_blank"
+							}
+						},
+						tooltip: (e, cell) =>  cell.getData().datei.titel	// Overwrite table option tooltip, which will return the datei-object
+					},
+					{title: 'Anmerkung', field: 'anmerkung', headerFilter: true},
+					// TODO value array als function oder als data?
+					{
+						title: 'Liste',
+						field: 'liste',
+						editor: "list",
+						editorParams:{values:['Neu', 'Genehmigt', 'Abgelehnt']},
+						headerFilter: true,
+						headerFilterParams:{values:['Neu', 'Genehmigt', 'Abgelehnt']},
+						frozen: true
+					},
+					{
+						title: 'Aktionen',
+						field: 'actions',
+						width: 105,	// Ensures Action-buttons will be always fully displayed
+						minWidth: 105,	// Ensures Action-buttons will be always fully displayed
+						maxWidth: 105,	// Ensures Action-buttons will be always fully displayed
+						formatter: (cell, formatterParams, onRendered) => {
+							let container = document.createElement('div');
+							container.className = "d-flex gap-2";
+
+							let button = document.createElement('button');
+							button.className = 'btn btn-outline-secondary';
+							button.innerHTML = '...';
+							button.addEventListener('click', () =>
+								this.manipulateData(cell.getRow().getIndex())
+							);
+							container.append(button);
+
+							button = document.createElement('button');
+							button.className = 'btn btn-outline-secondary';
+							button.innerHTML = '<i class="fa fa-edit"></i>';
+							button.addEventListener('click', (event) =>
+								this.editData(cell.getRow().getIndex())
+							);
+							container.append(button);
+
+							button = document.createElement('button');
+							button.className = 'btn btn-outline-secondary';
+							button.innerHTML = '<i class="fa fa-xmark"></i>';
+							button.addEventListener('click', () =>
+								this.deleteData(cell.getRow().getIndex())
+							);
+							container.append(button);
+
+							return container;
+						},
+						frozen: true
+					}
+				]
+			}
+		}
+	},
+	template: `
+	<div class="app-example-tables-basic">
+		<core-filter-cmpt 
+			ref="myTabulator"
+			table-only
+			:side-menu="false"
+			:tabulator-options="tabulatorOptions"
+			new-btn-label="Datensatz"
+			new-btn-show
+			new-btn-class="btn-primary"
+			@click:new="addData"
+			reload
+			>
+			<template #actions>
+				<button class="btn btn-primary" @click="acceptData">Datensatz genehmigen</button>
+				<button class="btn btn-danger" @click="rejectData">Datensatz ablehnen</button>
+			 	<div class="d-flex gap-2 align-items-baseline">
+					<select class="form-select" aria-label="Default select example">
+	  					<option selected>Aktion wählen...</option>
+						<option value="1">Status zurücksetzen</option>
+					  	<option value="2">Datensatz drucken</option>
+					  	<option value="3">Datensatz löschen</option>
+					</select>
+				</div>
+			</template>
+		</core-filter-cmpt>
+		
+		<!-- Modal -->
+		<bs-modal ref="modalContainer" class="bootstrap-prompt" v-bind="$props" @hidden-bs-modal="onHiddenBsModal">
+			<template #title>{{ modalTitel }}</template>
+			<template #default>Content</template>
+			<template #footer>
+				<button type="button" class="btn btn-primary" @click="onBsModalSave">{{ modalTitel }}</button>
+			</template>
+		</bs-modal>
+	</div>`
+});
+
+app
+	// Use Plugins:
+	// ===========
+	// FhcAlert
+	.use(FhcAlert)
+
+	// Start the App:
+	// =============
+	.mount('#example-tables-basic');
---
+++ views/Examples/VueJs/Tables.php
@@ -0,0 +1,233 @@
+<?php
+	$includesArray = [
+		'title' => 'My Extension - VueJs Examples - Tables',
+		'vue3' => true,
+		'axios027' => true,
+		'bootstrap5' => true,
+		'fontawesome6' => true,
+		'primevue3' => true,
+		'tabulator5' => true,
+		'navigationcomponent' => true,
+		'filtercomponent' => true,
+		'customCSSs' => [
+			'public/extensions/FHC-Core-Extension/css/VueJs.css'
+		],
+		'customJSModules' => [
+			// Load Vue Apps
+			'public/extensions/FHC-Core-Extension/js/apps/Examples.js',
+			'public/extensions/FHC-Core-Extension/js/apps/examples/Tables/Basic.js'
+		]
+	];
+	$this->load->view('templates/FHC-Header', $includesArray);
+?>
+
+	<div id="main">
+		<div id="nav">
+			<core-navigation-cmpt></core-navigation-cmpt>
+		</div>
+
+		<div id="content" class="row flex-row-reverse">
+			<header class="fhc-header">
+				<h1>VueJs Examples<small>Tables</small></h1>
+			</header>
+			<aside class="col-lg-3">
+				<div id="sidenav" class="list-group sticky-lg-top">
+					<a href="#table" class="list-group-item list-group-item-action">Tabulator without Filter (table-only)</a>
+					<a href="#table-structure" class="list-group-item list-group-item-action">- Basic structure</a>
+					<a href="#table-options" class="list-group-item list-group-item-action">- Tabulator Options</a>
+				</div>
+			</aside>
+			<main class="col-lg-9">
+				<h2 class="h3" id="table">Tabulator without Filter (table-only)</h2>
+				<p class="lead">
+					Tabulator without Filter will render your table with the select columns functionality ( <i class="fa fa-table-columns"></i> )  by default.<br>
+					You can easily add an Add-Button, Refresh-Button and Action-Buttons to handle multiple rows at once. Column formatters are used to keep same look&feel.
+				</p>
+				<div class="d-flex justify-content-between">
+					<h6>Example:</h6>
+					<a href="#" title="FHC-Core-Extension/public/js/apps/examples/Tables/Basic.js" data-bs-toggle="tooltip" data-bs-placement="left">
+						<i class="fa-solid fa-circle-info"></i>
+					</a>
+				</div>
+				<section class="border p-3">
+					<div id="example-tables-basic"></div>
+				</section>
+
+				<h3 class="h4" id="table-structure">Basic structure: Table-only Tabulator</h3>
+				<pre class="border"><code class="language-js"><?= htmlentities(
+					'// Import the Core Filter- and Core RESTClient Component to build your table and handle data' . "\n" .
+					'import {CoreFilterCmpt} from \'../../../../../js/components/filter/Filter.js\';' . "\n" .
+					'import {CoreRESTClient} from \'../../../../../../public/js/RESTClient.js\';' . "\n" .
+					"\n" .
+					'export default {' . "\n" .
+					'	components: {' . "\n" .
+					'		CoreFilterCmpt' . "\n" .
+					'	},' . "\n" .
+					'	methods: {' . "\n" .
+					'		setInitTableData() {' . "\n" .
+					'			CoreRESTClient' . "\n" .
+					'				.get(\'/extensions/FHC-Core-Extension/FhcTemplate/getTestData\')' . "\n" .
+					'				.then(result => result.data)' . "\n" .
+					'				.then(result => { this.$refs.myTabulator.tabulator.setData(CoreRESTClient.getData(result))} )' . "\n" .
+					'				.catch(this.$fhcAlert.handleSystemError);' . "\n" .
+					'		}' . "\n" .
+					'	},' . "\n" .
+					'	data() {' . "\n" .
+					'		return {' . "\n" .
+					'			// See below the tabulator options and columns example' . "\n" .
+					'			tabulatorOptions: { ... }' . "\n" .
+					'		};' . "\n" .
+					'	},' . "\n" .
+					'	// On mounted: Set initial table data' . "\n" .
+					'	mounted() {' . "\n" .
+					'		// Be sure the table was built before setting data' . "\n" .
+					'		this.$refs.myTabulator.tabulator.on(\'tableBuilt\', (e, row) => {' . "\n" .
+					'			this.setInitTableData();' . "\n" .
+					'		}' . "\n" .
+					'	},' . "\n" .
+					'	// Tabulator (table-only)' . "\n" .
+					'	template: `' . "\n" .
+					'		<core-filter-cmpt' . "\n" .
+					'			ref="myTabulator"' . "\n" .
+					'			table-only' . "\n" .
+					'			:side-menu="false"' . "\n" .
+					'			:tabulator-options="tabulatorOptions"' . "\n" .
+					'			>' . "\n" .
+					'		</core-filter-cmpt>`' . "\n" .
+
+					'};'
+				); ?></code></pre>
+
+				<h3 class="h4" id="table-options">Tabulator Options: Basic Example</h3>
+				<pre class="border"><code class="language-js"><?= htmlentities(
+					'// Basic Example for your Tabulator Options and how to format your columns depending on their data type' . "\n" .
+					'tabulatorOptions: {' . "\n" .
+					'	index: \'id\',							// Important! Tell tabulator the unique id\'s column field. Use it then with getIndex() method.' . "\n" .
+					'	maxHeight: "100%",' . "\n" .
+					'	minHeight: 100,' . "\n" .
+					'	layout: \'fitColumns\',					// fitColumns (default) squeezes into table width. fitData adds scrollbar if needed.' . "\n" .
+					'	rowHeight: 60,							// Condense table size if needed.' . "\n" .
+					'	placeholder: "Keine Daten vorhanden",		// If no data is provided, show this text' . "\n" .
+					'	columnDefaults: {' . "\n" .
+					'		tooltip: true,						// Use tooltip on your columns to display long text like Anmerkungen / Notizen / etc.' . "\n" .
+					'	},' . "\n" .
+					'	columns: [' . "\n" .
+					'		{' . "\n" .
+					'			formatter: \'rowSelection\',' . "\n" .
+					'			titleFormatter: \'rowSelection\',' . "\n" .
+					'			titleFormatterParams: {' . "\n" .
+					'				rowRange: "active"			// Only toggle the values of the active filtered rows' . "\n" .
+					'			},' . "\n" .
+					'			frozen: true,' . "\n" .
+					'			width: 70' . "\n" .
+					'		},' . "\n" .
+					'		{title: \'ID\', field: \'id\', headerFilter: true, width: 70, hozAlign: \'right\', frozen: true},' . "\n" .
+					'		{title: \'Core Data\', field: \'coreData\', headerFilter: true, frozen: true},' . "\n" .
+					'		{title: \'Integer\', field: \'number\', headerFilter: true, hozAlign: \'right\'},' . "\n" .
+					'		{title: \'String\', field: \'text\', headerFilter: true},' . "\n" .
+					'		{title: \'Date\', field: \'datum\', headerFilter: true, hozAlign: \'center\'},' . "\n" .
+					'		{' . "\n" .
+					'			title: \'Boolean\',' . "\n" .
+					'			field: \'bool\',' . "\n" .
+					'			formatter: "tickCross",' . "\n" .
+					'			headerFilter: "tickCross",' . "\n" .
+					'			headerFilterParams: {"tristate": true},' . "\n" .
+					'			hozAlign: "center",' . "\n" .
+					'			formatterParams: {' . "\n" .
+					'				tickElement: \'<i class="fa fa-check text-success"></i>\',' . "\n" .
+					'				crossElement: \'<i class="fa fa-xmark text-danger"></i>\'' . "\n" .
+					'			}' . "\n" .
+					'		},' . "\n" .
+					'		{' . "\n" .
+					'			title: "Money",' . "\n" .
+					'			field: "money",' . "\n" .
+					'			formatter: "money",' . "\n" .
+					'			formatterParams: {' . "\n" .
+					'				decimal: ",",' . "\n" .
+					'				thousand: ".",' . "\n" .
+					'				symbol: "€",' . "\n" .
+					'				symbolAfter: true' . "\n" .
+					'			}' . "\n" .
+					'		},' . "\n" .
+					'		{' . "\n" .
+					'			title: \'File\',' . "\n" .
+					'			field: \'datei\',' . "\n" .
+					'			headerFilter: true,' . "\n" .
+					'			formatter: "link",' . "\n" .
+					'			formatterParams: cell => {' . "\n" .
+					'				return {' . "\n" .
+					'					labelField: "datei.titel",' . "\n" .
+					'					url: FHC_JS_DATA_STORAGE_OBJECT.app_root +' . "\n" .
+					'						FHC_JS_DATA_STORAGE_OBJECT.ci_router +' . "\n" .
+					'						"/" +' . "\n" .
+					'						FHC_JS_DATA_STORAGE_OBJECT.called_path +' . "\n" .
+					'						"/download?dms_id=" +' . "\n" .
+					'						cell.getData().datei.dms_id,' . "\n" .
+					'					target: "_blank"' . "\n" .
+					'				}' . "\n" .
+					'			},' . "\n" .
+					'			tooltip: (e, cell) => cell.getData().datei.titel  // Overwrite table option tooltip, which will return the datei-object' . "\n" .
+					'		},' . "\n" .
+					'		{title: \'Anmerkung\', field: \'anmerkung\', headerFilter: true},' . "\n" .
+					'		{' . "\n" .
+					'			title: \'Liste\',' . "\n" .
+					'			field: \'liste\',' . "\n" .
+					'			editor: "list",' . "\n" .
+					'			editorParams: {values: [\'Neu\', \'Genehmigt\', \'Abgelehnt\']},' . "\n" .
+					'			headerFilter: true,' . "\n" .
+					'			headerFilterParams: {values: [\'Neu\', \'Genehmigt\', \'Abgelehnt\']},' . "\n" .
+					'			frozen: true' . "\n" .
+					'		},' . "\n" .
+					'		{' . "\n" .
+					'			title: \'Aktionen\',' . "\n" .
+					'			field: \'actions\',' . "\n" .
+					'			minWidth: 150,  // Ensures Action-buttons will be always fully displayed' . "\n" .
+					'			formatter: (cell, formatterParams, onRendered) => {' . "\n" .
+					'				let container = document.createElement(\'div\');' . "\n" .
+					'				container.className = "d-flex gap-2";' . "\n" .
+					"\n" .
+					'				let button = document.createElement(\'button\');' . "\n" .
+					'				button.className = \'btn btn-outline-secondary btn-action\';' . "\n" .
+					'				button.innerHTML = \'...\';' . "\n" .
+					'				button.addEventListener(' . "\n" .
+					'					\'click\',' . "\n" .
+					'					() => this.manipulateData(cell.getRow().getIndex())' . "\n" .
+					'				);' . "\n" .
+					'				container.append(button);' . "\n" .
+					"\n" .
+					'				button = document.createElement(\'button\');' . "\n" .
+					'				button.className = \'btn btn-outline-secondary btn-action\';' . "\n" .
+					'				button.innerHTML = \'<i class="fa fa-edit"></i>\';' . "\n" .
+					'				button.addEventListener(' . "\n" .
+					'					\'click\',' . "\n" .
+					'					event => this.editData(cell.getRow().getIndex())' . "\n" .
+					'				);' . "\n" .
+					'				container.append(button);' . "\n" .
+					"\n" .
+					'				button = document.createElement(\'button\');' . "\n" .
+					'				button.className = \'btn btn-outline-secondary btn-action\';' . "\n" .
+					'				button.innerHTML = \'<i class="fa fa-xmark"></i>\';' . "\n" .
+					'				button.addEventListener(' . "\n" .
+					'					\'click\',' . "\n" .
+					'					() => this.deleteData(cell.getRow().getIndex())' . "\n" .
+					'				);' . "\n" .
+					'				container.append(button);' . "\n" .
+					"\n" .
+					'				return container;' . "\n" .
+					'			},' . "\n" .
+					'			frozen: true' . "\n" .
+					'		}' . "\n" .
+					'	]' . "\n" .
+					'}'
+				); ?></code></pre>
+			</main>
+		</div>
+	</div>
+
+	<style type="text/css">
+		@import url("https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/default.min.css");
+	</style>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js"></script>
+	<script>hljs.highlightAll();</script>
+
+<?php $this->load->view('templates/FHC-Footer', $includesArray); ?>
