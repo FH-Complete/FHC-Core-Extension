624a8ef05654f4e844d5cecbc7a80c331ba5688d
Cris <hainberg@technikum-wien.at>
2024-02-20 17:28:30 +0100

    Added methods Examples-Controller to handle Exampledata and Examplestatus

--- controllers/Examples.php
+++ controllers/Examples.php
@@ -15,8 +15,15 @@ class Examples extends Auth_Controller
 			'viewExampleTabulatorOnly' => 'admin:rw',
 			'viewExampleTabulatorFilter' => 'admin:rw',
 			'viewIcons' => 'admin:rw',
-			'vuejs' => 'admin:rw'
+			'vuejs' => 'admin:rw',
+			'getExampledata' => 'admin:rw',
+			'getExamplestatusList' => 'admin:rw',
+			'updateExamplestatus' => 'admin:rw',
+			'deleteExampledata' => 'admin:rw'
 		]);
+
+		$this->load->model('extensions/FHC-Core-Extension/Exampledata_model', 'ExampledataModel');
+		$this->load->model('extensions/FHC-Core-Extension/Examplestatus_model', 'ExamplestatusModel');
 	}
 
 	/**
@@ -62,6 +69,82 @@ class Examples extends Auth_Controller
 	{
 		$example = $example ? '/' . ucfirst($example) : '';
 		$this->load->view('extensions/FHC-Core-Extension/Examples/VueJs' . $example);
+	}
+
+	public function getExampledata()
+	{
+		// Get Exampledata
+		$this->ExampledataModel->addSelect('
+			exampledata_id, 
+			uid, stringval,
+			integerval,
+			dateval,
+			booleanval, 
+			moneyval,
+			dokument_bezeichnung,
+			dms_id,
+			textval, 
+			examplestatus_kurzbz,
+			bezeichnung[(' . $this->_getLanguageIndex() . ')]');
+		$this->ExampledataModel->addJoin('extension.tbl_examplestatus', 'examplestatus_kurzbz');
+		$result = $this->ExampledataModel->load();
+
+		// On error
+		if (isError($result)) $this->terminateWithJsonError(getError($result));
+
+		// On success
+		$this->outputJsonSuccess(hasData($result) ? getData($result) : []);
+	}
+
+	public function getExamplestatusList()
+	{
+		// Get all Examplestatus
+		$this->ExamplestatusModel->addSelect('examplestatus_kurzbz, bezeichnung[(' . $this->_getLanguageIndex() . ')]');
+		$result = $this->ExamplestatusModel->load();
+
+		// On error
+		if (isError($result)) $this->terminateWithJsonError(getError($result));
+
+		// On success
+		$this->outputJsonSuccess(hasData($result) ? getData($result) : []);
+	}
+
+	public function updateExamplestatus()
+	{
+		$data = $this->getPostJson();
+
+		$result = $this->ExampledataModel->update(
+			$data->exampledata_id,
+			array('examplestatus_kurzbz' => $data->examplestatus_kurzbz)
+		);
+
+		// On error
+		if (isError($result)) $this->terminateWithJsonError(getError($result));
+
+		// On success
+		$this->outputJsonSuccess(hasData($result) ? getData($result) : []);
+	}
+
+	public function deleteExampledata(){
+		$data = $this->getPostJson();
+
+		$result = $this->ExampledataModel->delete($data->exampledata_id);
+
+		// On error
+		if (isError($result)) $this->terminateWithJsonError(getError($result));
+
+		// On success
+		$this->outputJsonSuccess(hasData($result) ? getData($result) : []);
+	}
+
+	private function _getLanguageIndex()
+	{
+		$this->load->model('system/Sprache_model', 'SpracheModel');
+		$this->SpracheModel->addSelect('index');
+		$result = $this->SpracheModel->loadWhere(array('sprache' => getUserLanguage()));
+
+		// Return language index
+		return hasData($result) ? getData($result)[0]->index : 1;
 	}
 }
 
