9d4da79da47f0a784474308accb6f36fb8b6f8f3
cgfhtw <christopher.gerbrich@technikum-wien.at>
2024-02-20 08:37:35 +0100

    Form Full Example

--- public/js/apps/examples/Form/Full.js
+++ public/js/apps/examples/Form/Full.js
@@ -35,6 +35,8 @@ const app = Vue.createApp({
 	},
 	data() {
 		return {
+			autocompleteAbortController: null,
+			autocompleteSuggestions: [],
 			formData: {
 				mystring: '',
 				mypassword: '',
@@ -42,29 +44,68 @@ const app = Vue.createApp({
 				mytext: '',
 				myselect: '',
 				mysinglechoice: 'yes',
-				mymultiplechoice: ['apple']
+				mymultiplechoice: ['apple'],
+				mycheckbox: false,
+				myswitch: true,
+				myrange: 10,
+				mycolor: '#ff0000',
+				myautocomplete: '',
+				mydate: '',
+				myfile: [],
+				mydmsfiles: []
 			}
 		};
 	},
 	methods: {
+		loadAutocompleteSuggestions(event) {
+			if (event.query || !this.autocompleteSuggestions.length) {
+				if (this.autocompleteAbortController)
+					this.autocompleteAbortController.abort();
+				this.autocompleteAbortController = new AbortController();
+
+				this.$fhcApi
+					.get(
+						'extensions/FHC-Core-Extension/components/form/autocompleteSuggestions/' + encodeURIComponent(event.query),
+						null,
+						{
+							signal: this.autocompleteAbortController.signal
+						}
+					)
+					.then(result => this.autocompleteSuggestions = result.data)
+					.catch(this.$fhcAlert.handleSystemError);
+			}
+		},
 		sendForm() {
-			if (this.$refs.form) // We can only send when $refs is ready
+			// We can only send when $refs is ready
+			if (this.$refs.form) {
+				
+				// copy the formData object to manipulate some data
+				const formData = {...this.formData};
+
+				// check if myautocomplete is an object and flatten it
+				// down to the primary index because that's what the
+				// endpoint expects.
+				if (formData.myautocomplete.nation_code)
+					formData.myautocomplete = formData.myautocomplete.nation_code;
+
+				// send the data
 				this.$refs.form
 					.post(
 						'extensions/FHC-Core-Extension/components/form/full',
-						this.formData
+						formData
 					)
 					.then(result => {
 						this.$fhcAlert.alertSuccess('Form Successful sent');
 					})
 					.catch(this.$fhcAlert.handleSystemError);
+			}
 		}
 	},
 	template: `
 	<div class="app-example-form-1">
 		<fhc-form ref="form" @submit.prevent="sendForm">
 			<form-validation></form-validation>
-			<div class="row row-cols-3">
+			<div class="row row-cols-3 g-3">
 				<div class="col">
 					<form-input
 						v-model="formData.mystring"
@@ -94,12 +135,13 @@ const app = Vue.createApp({
 					</form-input>
 					<div class="form-text">A number less than 10</div>
 				</div>
-				<div class="col">
+				<div class="col-8">
 					<form-input
 						type="textarea"
 						v-model="formData.mytext"
 						name="mytext"
 						label="MyText"
+						rows="4"
 						>
 					</form-input>
 					<div class="form-text text-end" :class="{'text-danger': formData.mytext.length > 10}">{{formData.mytext.length}}/10</div>
@@ -173,8 +215,92 @@ const app = Vue.createApp({
 					</form-input>
 					<div class="form-text">Select at least two.</div>
 				</div>
+				<div class="col">
+					<div>MyCheckbox*</div>
+					<form-input
+						type="checkbox"
+						v-model="formData.mycheckbox"
+						name="mycheckbox"
+						label="I Agree"
+						>
+					</form-input>
+				</div>
+				<div class="col">
+					<div>MySwitch*</div>
+					<form-input
+						type="checkbox"
+						v-model="formData.myswitch"
+						name="myswitch"
+						label="Show Something"
+						container-class="form-switch"
+						>
+					</form-input>
+				</div>
+				<div class="col-8">
+					<form-input
+						type="range"
+						v-model="formData.myrange"
+						name="myrange"
+						label="MyRange"
+						min="0"
+						max="15"
+						>
+					</form-input>
+				</div>
+				<div class="col">
+					<form-input
+						type="color"
+						v-model="formData.mycolor"
+						name="mycolor"
+						label="MyColor"
+						>
+					</form-input>
+				</div>
+				<div class="col">
+					<form-input
+						type="autocomplete"
+						v-model="formData.myautocomplete"
+						name="myautocomplete"
+						label="MyAutocomplete*"
+						dropdown
+						:suggestions="autocompleteSuggestions"
+						@complete="loadAutocompleteSuggestions"
+						option-label="kurztext"
+						>
+					</form-input>
+				</div>
+				<div class="col">
+					<form-input
+						type="datepicker"
+						v-model="formData.mydate"
+						name="mydata"
+						label="MyDate"
+						>
+					</form-input>
+				</div>
+				<div class="col-6">
+					<form-input
+						type="uploadfile"
+						v-model="formData.myfile"
+						name="myfile"
+						label="MyFile"
+						>
+					</form-input>
+				</div>
+				<div class="col-6">
+					<form-input
+						type="uploaddms"
+						v-model="formData.mydmsfiles"
+						name="mydmsfiles"
+						label="MyDmsFiles"
+						multiple
+						>
+					</form-input>
+				</div>
 			</div>
-			<button type="submit" class="btn btn-primary">Send Form</button>
+			<div class="mt-3">
+				<button type="submit" class="btn btn-primary">Send Form</button>
+			</div>
 		</fhc-form>
 	</div>`
 });
